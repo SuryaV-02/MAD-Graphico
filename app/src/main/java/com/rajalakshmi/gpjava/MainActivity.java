bc36e9ca13bceec61dd89900c68ed805ae9c62b7
Surya Venkatesan <suryaedu71@gmail.com>
2022-03-27 22:03:50 +0530

    Added Line

--- app/src/main/java/com/rajalakshmi/gpjava/MainActivity.java
+++ app/src/main/java/com/rajalakshmi/gpjava/MainActivity.java
@@ -30,21 +30,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected  void onDraw(Canvas canvas){
             Paint paint = new Paint();
-            paint.setColor(Color.YELLOW);
+            paint.setColor(Color.BLACK);
             canvas.drawPaint(paint);
-            paint.setColor(getResources().getColor(R.color.Orange));
+            paint.setColor(Color.WHITE);
             paint.setTextSize(50f);
             canvas.drawText("Graphical Primitives",300,80,paint);
-            paint.setColor(Color.RED);
+            paint.setColor(getResources().getColor(R.color.Gold));
             paint.setTextSize(50f);
             canvas.drawText("Circle",160,200,paint);
             canvas.drawCircle(250,500,200,paint);
-            paint.setColor(getResources().getColor(R.color.Rose));
+            paint.setColor(getResources().getColor(R.color.Purple));
             canvas.drawText("Rectangle",600,200,paint);
             canvas.drawRect(600,300,900,900,paint);
-            paint.setColor(getResources().getColor(R.color.DeepBlue));
-            canvas.drawText("Oval",160,1000,paint);
-            canvas.drawOval(100,1050,500,1600,paint);
+            paint.setColor(getResources().getColor(R.color.Pink));
+            canvas.drawText("Line",160,1000,paint);
+            canvas.drawLine(100,1050,500,1600,paint);
+            paint.setColor(getResources().getColor(R.color.Skies));
+            canvas.drawText("Oval",600,1000,paint);
+            canvas.drawOval(600,1050,800,1500,paint);
         }
     }
 }
